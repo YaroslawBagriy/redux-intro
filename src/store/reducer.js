e5932969c41827967720178c0ad67505a4ef2b74
yaroslawbagriy <yaroslawbagriy@gmail.com>
2020-05-12 08:24:01 -0500

    first commit

---
+++ src/store/reducer.js
@@ -0,0 +1,44 @@
+const initialState = {
+     counter: 0,
+     results: []
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                ...state,
+                counter: state.counter + 1
+            }
+        case 'DECREMENT':
+            return {
+                ...state,
+                counter: state.counter + 1
+            }
+        case 'ADD':
+            return {
+                ...state,
+                counter: state.counter + action.val
+            }
+        case 'SUBTRACT':
+            return {
+                ...state,
+                counter: state.counter - action.val
+            }
+        case 'STORE_RESULT':
+            return {
+                ...state,
+                results: state.results.concat({id: new Date(), value: state.counter})
+            }
+        case 'DELETE_RESULT':
+            const updatedArray = state.results.filter((result, index) => result.id !== action.id);
+            return {
+                ...state,
+                results: updatedArray
+            }
+    }
+
+    return state;
+}
+
+export default reducer;
